068d24650930287c4017da612d1731231d8a4879
ALI Djamel <djamel.ali1920@gmail.com>
2021-03-28 20:46:01 +0200

    launch of the "lineFollower" program from the "MainClass" class, some adjustments of the PID proportionality constants.

--- ev3dev-lang-java/src/main/java/ev3dev/functionalities/LineFollower.java
+++ ev3dev-lang-java/src/main/java/ev3dev/functionalities/LineFollower.java
@@ -16,8 +16,8 @@ import static ev3dev.sensors.Button.ENTER;
 public class LineFollower {
     // Attributes :
     private final static int NB_OF_COLORS_TO_LEARN = 3;
-    private final static int NB_OF_MEASURES_PER_COLOR = 6;
-    private static int I_AM_IN_LINE = 1; // (it is -1 when it is not on the line)
+    private final static int NB_OF_MEASURES_PER_COLOR = 3;
+    private static int I_AM_IN_LINE = 1; // (it's equal to 1 when he's on the line, -1 on the background, 0 on the frontier)
     private static final double MAXIMUM_TOLERATED_DISTANCE = 110;
     private static float[] sample;
     private final SampleProvider sampleProvider;
@@ -25,11 +25,10 @@ public class LineFollower {
     private final LearningColors learningColors;
     private ArrayList<Color> listOfLearnedColors;
     private static Color lineColor, backgroundColor, medianColor, calculatedMedianColor, capturedMedianColor;
-//    private Motors motors;
+    //   Motors;
+    private EV3LargeRegulatedMotor rightMotor;
+    private EV3LargeRegulatedMotor leftMotor;
 
-    //Robot Definition (and initialization):
-    private static EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
-    private static EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.D);
     private int leftMotorSpeed, rightMotorSpeed, motorSpeed, targetPower;
 
     // Distance between the line color and the background color.
@@ -39,6 +38,8 @@ public class LineFollower {
         learningColors = new LearningColors();
         listOfLearnedColors = new ArrayList<>(NB_OF_COLORS_TO_LEARN);
         distanceLineBackground = 0;
+        rightMotor = new EV3LargeRegulatedMotor(MotorPort.D);
+        leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
         ev3ColorSensor = new EV3ColorSensor(SensorPort.S1);
         sampleProvider = ev3ColorSensor.getRGBMode();
         int sampleSize = sampleProvider.sampleSize();
@@ -61,7 +62,7 @@ public class LineFollower {
         rightMotor.setSpeed(motorSpeed);
     }
 
-    public static void main(String[] args) {
+    public void followTheLine() {
         // * Proportionality constant is the factor that you have to multiply the error (x value) by
         //   to convert it into a Turn (y value).
         // * The turns range from -1 (hard turn to the left) to +1 (hard turn to the right)
@@ -69,6 +70,10 @@ public class LineFollower {
         double turn, error, proportionality_constant/*, ki = 0.05*/;
         int target_power/*, integral = 0*/;
         String the_closest_color;
+
+        // initialization of motors:
+        EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
+        EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.D);
 
         // Initialization
         LineFollower lineFollower = new LineFollower();
@@ -78,7 +83,7 @@ public class LineFollower {
         // End of the learning phase
 
         // get the list of learned colors
-       lineFollower.listOfLearnedColors = LearningColors.getListOfLearnedColors();
+        lineFollower.listOfLearnedColors = LearningColors.getListOfLearnedColors();
 
         // Color identification
         lineColor = lineFollower.listOfLearnedColors.get(0);
@@ -97,9 +102,9 @@ public class LineFollower {
 //        medianColor = new Color("FRONTIER", f);
 
         System.out.println("\n\n\t Affichages:");
-        System.out.println("\t LINE : " +  lineColor);
-        System.out.println("\t BACKGROUND : " +  backgroundColor);
-        System.out.println("\t FRONTIER : " +  medianColor);
+        System.out.println("\t LINE : " + lineColor);
+        System.out.println("\t BACKGROUND : " + backgroundColor);
+        System.out.println("\t FRONTIER : " + medianColor);
 
         // the max distance is the average of the two distances color of the line - color of the border
         // and color of the background - color of the border
@@ -127,13 +132,13 @@ public class LineFollower {
 //        lineFollower.distanceLineBackground = Color.getDistance(lineColor.getRgbValues(), backgroundColor.getRgbValues());
 //        lineFollower.offset = lineFollower.distanceLineBackground/2;
 
-        // We'll guess that we want the power to go from ' motor.getMaxSpeed()/2 ' to 0 when the error goes from
+        // We'll guess that we want the power to go from for e.g: ' motor.getMaxSpeed()/2 ' to 0 when the error goes from
         // 0 to ' - max_distance '. That means the Kp (the slope remember, the change in y divided by the change in x) is;
 
         //Kp = (0 - motor.getMaxSpeed()/2)/(- max_distance - 0)
         // in our case : proportionality_constant = (-target_power)/(-max_distance);
-        target_power = (int)getAverageMaxMotorSpeed(leftMotor ,rightMotor)/18;
-        proportionality_constant = 885*target_power/(1000*max_distance);
+        target_power = (int) getAverageMaxMotorSpeed(lineFollower.leftMotor, lineFollower.rightMotor) / 11;
+        proportionality_constant = 185 * target_power / (100 * max_distance);
         System.out.println("proportionality_constant" + proportionality_constant);
         System.out.println("target_power" + target_power);
         System.out.println("max_distance" + max_distance);
@@ -147,17 +152,17 @@ public class LineFollower {
         System.out.println("\n Start following the line : ");
 
         // While the brick is detecting
-        while (Button.ESCAPE.isUp()){
+        while (Button.ESCAPE.isUp()) {
             // Detect a color and get the RGB values
             lineFollower.sampleProvider.fetchSample(sample, 0);
-            System.out.println("\t SAMPLE : R = " +  sample[0] + "; G = "+ sample[1] + "; B = "+ sample[2]);
+            System.out.println("\t SAMPLE : R = " + sample[0] + "; G = " + sample[1] + "; B = " + sample[2]);
 
             // what is the current color reading ?
             // Calculate the distances (in order to find out which color this sample is closest to (line or background or other).
             the_closest_color = Color.getTheClosestColor(sample, backgroundColor, lineColor, medianColor, MAXIMUM_TOLERATED_DISTANCE);
-            double dis = Color.getDistance(sample, medianColor.getRgbValues());
-            System.out.println("\t Closest_color-Median DISTANCE == " + dis);
-            System.out.println("\t the_closest_color found : " +  the_closest_color);
+            double sample_median_distance = Color.getDistance(sample, medianColor.getRgbValues());
+            System.out.println("\t Sample-Median Distance == " + sample_median_distance);
+            System.out.println("\t the_closest_color found : " + the_closest_color);
 
 //            if(the_closest_color.equalsIgnoreCase("OTHER")){
 //                System.out.println("\n\n\t COLOR == OTHER \n");
@@ -170,78 +175,73 @@ public class LineFollower {
 ////                return;
 //            }
 //            else{
-                if(the_closest_color.equalsIgnoreCase(lineColor.getName())) I_AM_IN_LINE= 1;
-                else if(the_closest_color.equalsIgnoreCase(backgroundColor.getName()) ) I_AM_IN_LINE= -1;
-                if(the_closest_color.equalsIgnoreCase(medianColor.getName()) || the_closest_color.equalsIgnoreCase("OTHER")) I_AM_IN_LINE= 0;
+            if (the_closest_color.equalsIgnoreCase(lineColor.getName())) I_AM_IN_LINE = 1;
+            else if (the_closest_color.equalsIgnoreCase(backgroundColor.getName())) I_AM_IN_LINE = -1;
+            if (the_closest_color.equalsIgnoreCase(medianColor.getName()) || the_closest_color.equalsIgnoreCase("OTHER"))
+                I_AM_IN_LINE = 0;
 
-                // calculate the error
-                error = Color.getDistance(sample, medianColor.getRgbValues())/2 * I_AM_IN_LINE;
+            // calculate the error
+            error = sample_median_distance * I_AM_IN_LINE;
 
-                // update ki
-                //integral+= error;
+            // update ki
+            //integral+= error;
 
-                // the "P term", how much we want to change the motors' power
-                turn = proportionality_constant * error /*+ ki*integral*/;
+            // the "P term", how much we want to change the motors' power
+            turn = proportionality_constant * error /*+ ki*integral*/;
 
-                // the power level for the A motor
-                lineFollower.leftMotorSpeed = target_power + (int)turn;
+            // the power level for the A motor
+            lineFollower.leftMotorSpeed = target_power + (int) turn;
 
-                // the power level for the C motor
-                lineFollower.rightMotorSpeed = target_power - (int)turn;
+            // the power level for the C motor
+            lineFollower.rightMotorSpeed = target_power - (int) turn;
 
-                if(lineFollower.leftMotorSpeed > 0 && lineFollower.rightMotorSpeed > 0){
+            if (lineFollower.leftMotorSpeed > 0 && lineFollower.rightMotorSpeed > 0) {
+
+                // set speed (with the new power level)
+                lineFollower.leftMotor.setSpeed(lineFollower.leftMotorSpeed);
+                lineFollower.rightMotor.setSpeed(lineFollower.rightMotorSpeed);
+
+                // Make the motors move forward
+                lineFollower.leftMotor.forward();
+                lineFollower.rightMotor.forward();
+            } else {
+                if (lineFollower.leftMotorSpeed < 0 && lineFollower.rightMotorSpeed > 0) {
+
+                    lineFollower.leftMotorSpeed = -lineFollower.leftMotorSpeed;
 
                     // set speed (with the new power level)
-                    leftMotor.setSpeed(lineFollower.leftMotorSpeed);
-                    rightMotor.setSpeed(lineFollower.rightMotorSpeed);
+                    lineFollower.leftMotor.setSpeed(lineFollower.leftMotorSpeed);
+                    lineFollower.rightMotor.setSpeed(lineFollower.rightMotorSpeed);
 
                     // Make the motors move forward
-                    leftMotor.forward();
-                    rightMotor.forward();
+                    lineFollower.leftMotor.backward();
+                    lineFollower.rightMotor.forward();
+                } else if (lineFollower.rightMotorSpeed < 0 && lineFollower.leftMotorSpeed > 0) {
+
+                    lineFollower.rightMotorSpeed = -lineFollower.rightMotorSpeed;
+
+                    // set speed (with the new power level)
+                    lineFollower.leftMotor.setSpeed(lineFollower.leftMotorSpeed);
+                    lineFollower.rightMotor.setSpeed(lineFollower.rightMotorSpeed);
+
+                    lineFollower.rightMotor.backward();
+                    lineFollower.leftMotor.forward();
+                } else {
+                    lineFollower.leftMotorSpeed = -lineFollower.leftMotorSpeed;
+                    lineFollower.rightMotorSpeed = -lineFollower.rightMotorSpeed;
+
+                    // set speed (with the new power level)
+                    lineFollower.leftMotor.setSpeed(lineFollower.leftMotorSpeed);
+                    lineFollower.rightMotor.setSpeed(lineFollower.rightMotorSpeed);
+                    lineFollower.leftMotor.backward();
+                    lineFollower.rightMotor.backward();
                 }
-                else {
-                    if(lineFollower.leftMotorSpeed < 0 && lineFollower.rightMotorSpeed > 0){
-
-                        lineFollower.leftMotorSpeed = -lineFollower.leftMotorSpeed;
-
-                        // set speed (with the new power level)
-                        leftMotor.setSpeed(lineFollower.leftMotorSpeed);
-                        rightMotor.setSpeed(lineFollower.rightMotorSpeed);
-
-                        // Make the motors move forward
-                        leftMotor.backward();
-                        rightMotor.forward();
-                    }
-                    else if(lineFollower.rightMotorSpeed < 0 && lineFollower.leftMotorSpeed > 0){
-
-                        lineFollower.rightMotorSpeed = -lineFollower.rightMotorSpeed;
-
-                        // set speed (with the new power level)
-                        leftMotor.setSpeed(lineFollower.leftMotorSpeed);
-                        rightMotor.setSpeed(lineFollower.rightMotorSpeed);
-
-                        rightMotor.backward();
-                        leftMotor.forward();
-                    }
-                    else{
-                        lineFollower.leftMotorSpeed = -lineFollower.leftMotorSpeed;
-                        lineFollower.rightMotorSpeed = -lineFollower.rightMotorSpeed;
-
-                        // set speed (with the new power level)
-                        leftMotor.setSpeed(lineFollower.leftMotorSpeed);
-                        rightMotor.setSpeed(lineFollower.rightMotorSpeed);
-                        leftMotor.backward();
-                        rightMotor.backward();
-
-                    }
-                }
-
-
+            }
 //            }
         }
         // stop motors
-        leftMotor.stop();
-        rightMotor.stop();
+        lineFollower.leftMotor.stop();
+        lineFollower.rightMotor.stop();
     }
 
     static Color getMedianColor(Color _lineColor, Color _backgroundColor, Color _capturedMedianColor) {
@@ -258,7 +258,36 @@ public class LineFollower {
         return new Color("FRONTIER", medianColorRGB);
     }
 
-    static float getAverageMaxMotorSpeed(EV3LargeRegulatedMotor _leftMotor, EV3LargeRegulatedMotor _rightMotor){
-        return (_leftMotor.getMaxSpeed()+_rightMotor.getMaxSpeed())/2;
+    static float getAverageMaxMotorSpeed(EV3LargeRegulatedMotor _leftMotor, EV3LargeRegulatedMotor _rightMotor) {
+        return (_leftMotor.getMaxSpeed() + _rightMotor.getMaxSpeed()) / 2;
     }
 }
+//todo: learning phase
+// Measure 5 accepted
+//
+//
+//         MEASURE == 5,
+//         DISTANCE == 1.8110797237127985
+//         AVERAGE COLOR : Color[name='average_color', rgbValues=[97.5, 94.0, 64.833336]]
+//
+//         ACCEPTABLE_MEASURE[5] : Color[name='measure_5', rgbValues=[96.0, 94.0, 65.0]]
+//
+//         Summary of the average values of the learned colors :
+//         ____________________________________
+//         Color[name='COLOR_0', rgbValues=[13.5, 18.166666, 8.666667]]
+//         Color[name='COLOR_1', rgbValues=[153.33333, 151.5, 104.0]]
+//         Color[name='COLOR_2', rgbValues=[97.5, 94.0, 64.833336]]
+//
+//
+//         Affichages:
+//         LINE : Color[name='LINE', rgbValues=[13.5, 18.166666, 8.666667]]
+//         BACKGROUND : Color[name='BACKGROUND', rgbValues=[153.33333, 151.5, 104.0]]
+//         FRONTIER : Color[name='FRONTIER', rgbValues=[90.45833, 89.41667, 60.583336]]
+//         2021-02-23 16:30:53 [main] TRACE ev3dev.utils.Sysfs - cat /sys/class/power_supply/lego-ev3-battery/voltage_now
+//        2021-02-23 16:30:54 [main] TRACE ev3dev.utils.Sysfs - value: 7790133
+//        2021-02-23 16:30:54 [main] TRACE ev3dev.utils.Sysfs - cat /sys/class/power_supply/lego-ev3-battery/voltage_now
+//        2021-02-23 16:30:54 [main] TRACE ev3dev.utils.Sysfs - value: 7798133
+//        proportionality_constant10.28511790536103
+//        target_power136
+//        max_distance117.02345185295677
+
